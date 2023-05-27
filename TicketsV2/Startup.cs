5868b2b8cd4a8dc405b5071d90df1b2277b2fcf7
Diego <diego.ochoa.maldonado@hotmail.com>
2023-05-27 16:50:08 -0600

    updated startup cs

--- TicketsV2/Startup.cs
+++ TicketsV2/Startup.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using TicketsV2.Interfaces;
 using TicketsV2.Services;
+using System.Configuration;
 
 [assembly: FunctionsStartup(typeof(TicketsV2.Startup))]
 
@@ -18,9 +22,11 @@ namespace TicketsV2
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            builder.Services.AddDbContext<DBClient>(
-                options => SqlServerDbContextOptionsExtensions.UseSqlServer(options, Environment.GetEnvironmentVariable("SqlConnectionString")));
+            var connectionString = Environment.GetEnvironmentVariable("SqlConnectionString");
 
+            builder.Services.AddDbContext<DBClient>(
+              options => SqlServerDbContextOptionsExtensions.UseSqlServer(options, connectionString));
+            
             builder.Services.AddSingleton<ITicket, QRService>();
         }
     }
