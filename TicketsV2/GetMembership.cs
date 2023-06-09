8929e564007d4390ed01c783d3078640489ae796
Diego <diego.ochoa.maldonado@hotmail.com>
2023-06-08 23:37:39 -0600

    updated stripe checkout

---
+++ TicketsV2/GetMembership.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Stripe;
+using static QRCoder.PayloadGenerator;
+
+namespace TicketsV2
+{
+    public static class GetMembership
+    {
+        [FunctionName("GetMembership")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function,"post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            StripeConfiguration.ApiKey = Environment.GetEnvironmentVariable("StripeKey");
+
+            string customerId = await new StreamReader(req.Body).ReadToEndAsync();
+
+            var options = new CustomerGetOptions();
+            var service = new CustomerService();
+            options.AddExpand("subscriptions");
+           
+            var customer = service.Get("cus_O2ydWRgRvLIB7a", options);
+
+            
+            return new OkObjectResult(customer.Subscriptions.Data);
+        }
+    }
+}
+
---
+++ TicketsV2/Models/ProductStripe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TicketsV2.Models
+{
+	public class ProductStripe
+	{
+        public string ProductID { get; set; }
+		public string ProductName { get; set; }
+		public string ProductDescription { get; set; }
+
+		public ProductStripe()
+		{
+			
+		}
+	}
+}
+
